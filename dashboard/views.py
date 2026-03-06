995fef765dd61aeb3b967b4fab0f13ac19ce03a4
Yuri <yuriaragao0511@gmail.com>
2026-03-06 16:23:36 -0300

    Ajustes dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,69 +1,22 @@
 from django.shortcuts import render
-from control.models import Livro
 from django.contrib.auth.decorators import login_required
+from dashboard.models import Livros, Emprestimo
 
 @login_required
 def dashboard(request):
+    livros = Livros.objects.all()
+    emprestimos = Emprestimo.objects.all()
 
-    ## fazer funcao de contar os emprestimos ativos e inativos, e total de livros
-    emprestimos_ativos = 15
-    total_copias = 10
-    total_livros = 120
+    total_livros = livros.count()
+    total_copias = sum(l.quantidade for l in livros)
+    emprestimos_ativos = emprestimos.filter(status='Ativo').count()
 
-
-    ## funcao de carregar os todos livros na tela
-
-  
-    livros = Livro.objects.all()
-    total_livros = len(livros)
-    total_copias= total_livros
-    ## logica de carregar os livros na tela, e depois criar a função de contar os emprestimos ativos e inativos, e total de livros
-
-    emprestimos = [
-        {
-            'cliente': 'João Silva',
-            'livro': 'Dom Casmurro',
-            'data_emprestimo': '01/03/2026',
-            'data_devolucao': '15/03/2026',
-            'status': 'Ativo'
-        },
-        {
-            'cliente': 'Maria Oliveira',
-            'livro': '1984',
-            'data_emprestimo': '20/02/2026',
-            'data_devolucao': '06/03/2026',
-            'status': 'Pendente'
-        },
-        {
-            'cliente': 'Carlos Souza',
-            'livro': 'O Alquimista',
-            'data_emprestimo': '10/02/2026',
-            'data_devolucao': '24/02/2026',
-            'status': 'Devolvido'
-        },
-        {
-            'cliente': 'Ana Costa',
-            'livro': 'O Senhor dos Anéis',
-            'data_emprestimo': '02/03/2026',
-            'data_devolucao': '16/03/2026',
-            'status': 'Ativo'
-        },
-        {
-            'cliente': 'Ricardo Lima',
-            'livro': 'Harry Potter',
-            'data_emprestimo': '15/01/2026',
-            'data_devolucao': '29/01/2026',
-            'status': 'Atrasado'
-        },
-    ]
-
-    ## aqui vai cair a função de contar os emprestimos ativos e inativos, e total de livros
-    context={
+    context = {
         'livros': livros,
-        'emprestimos_ativos': emprestimos_ativos,
-        'total_copias': total_copias,
+        'emprestimos': emprestimos,
         'total_livros': total_livros,
-        'emprestimos': emprestimos
+        'total_copias': total_copias,
+        'emprestimos_ativos': emprestimos_ativos,
     }
 
     return render(request, "criando/criando.html", context)
